c4ac5d87afb68123bec3c32501819f2b385499f7
richwang <283247429@qq.com>
2014-05-20 21:33:38 +0800

    change send nxt to send NFD

--- html/ui/js/nrs.modals.accountdetails.js
+++ html/ui/js/nrs.modals.accountdetails.js
@@ -24,7 +24,7 @@ var NRS = (function(NRS, $, undefined) {
 
 			if (!NRS.accountInfo.publicKey) {
 				$("#account_balance_public_key").html("/");
-				$("#account_balance_warning").html("Your account does not have a public key! This means it's not as protected as other accounts. You must make an outgoing transaction to fix this issue. (<a href='#' data-toggle='modal' data-target='#send_message_modal'>send a message</a>, <a href='#' data-toggle='modal' data-target='#register_alias_modal'>buy an alias</a>, <a href='#' data-toggle='modal' data-target='#send_money_modal'>send Nxt</a>, ...)").show();
+				$("#account_balance_warning").html("Your account does not have a public key! This means it's not as protected as other accounts. You must make an outgoing transaction to fix this issue. (<a href='#' data-toggle='modal' data-target='#send_message_modal'>send a message</a>, <a href='#' data-toggle='modal' data-target='#register_alias_modal'>buy an alias</a>, <a href='#' data-toggle='modal' data-target='#send_money_modal'>send NFD</a>, ...)").show();
 			}
 		}
 	});

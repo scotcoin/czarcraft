af25f579897e8f506e7023b3d0e67b15d5d51a3b
Oliver Schiessl <info@alax.de>
2014-06-14 14:19:05 +0200

    new files

---
+++ html/ui/js/lang/lang.de.js
@@ -0,0 +1,6 @@
+{
+		"SEND_NFD":"Send NFD",
+		"SEND_MESSAGE":"Send Message",
+		"GENERATE_TOKEN":"Generate Token",
+		"SETTINGS":"Settings"
+}
---
+++ html/ui/js/lang/lang.en.js
@@ -0,0 +1,6 @@
+{
+		"SEND_NFD":"Sende NFD",
+		"SEND_MESSAGE":"Sende Message",
+		"GENERATE_TOKEN":"Generiere Token",
+		"SETTINGS":"Settings"
+}

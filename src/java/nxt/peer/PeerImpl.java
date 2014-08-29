a2203db8fd4a726ff129e4575ceb6bb372f41117
MaWo <mawo@ich-habe-fertig.com>
2014-08-30 00:37:53 +0200

    don't connect to old 1.1.4 peers; differentiate testnet and mainnet peers

--- src/java/nxt/peer/PeerImpl.java
+++ src/java/nxt/peer/PeerImpl.java
@@ -382,7 +382,7 @@ final class PeerImpl implements Peer {
                 setAnnouncedAddress(peerAddress);
                 Logger.logDebugMessage("Connected to peer without announced address, setting to " + peerAddress);
             }
-            if (analyzeHallmark(announcedAddress, (String)response.get("hallmark"))  && version!=null && (version.startsWith("1.1.4") || version.startsWith("NFD"))) {
+            if (analyzeHallmark(announcedAddress, (String)response.get("hallmark"))  && version!=null && ( Constants.isTestnet ? version.startsWith("NFD-TESTNET-") : version.startsWith("NFD-"))) {
                 setState(State.CONNECTED);
                 Peers.updateAddress(this);
             } else {

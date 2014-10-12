7ca720ca2e05c9cad2bb93dee0b33bc6f279631e
Treefunder Incorporated <ceo@treefunder.com>
2014-10-11 22:07:37 -0400

    Czarcraft Staging

--- win-compile.sh
+++ win-compile.sh
@@ -6,8 +6,8 @@ SP=src/java/
 
 javac -sourcepath $SP -classpath $CP -d classes/ src/java/nxt/*.java src/java/nxt/*/*.java || exit 1
 
-/bin/rm -f nfd.jar 
-jar cf nfd.jar -C classes . || exit 1
+/bin/rm -f tzr.jar 
+jar cf tzr.jar -C classes . || exit 1
 /bin/rm -rf classes
 
-echo "nfd.jar generated successfully"
+echo "tzr.jar generated successfully"
